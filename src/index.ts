3253df6de3d1a33b69f95bfdc3d352dad0c0d4e5
thilv43 <thilv44443333@gmail.com>
2022-03-14 16:52:50 +0700

    buoi 2 update

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+// export {};
+
+// function show<T,U>(a:T, b:U): [T, U]{
+//     return[a,b];
+// }
+// show(10, 20);
+// show("Le", "Dat");
+
+type Product =  {
+    id: number,
+    name: string,
+}
+
+const product: Product[] = [
+        {
+        
+        id: 1,
+        name: "Products A",
+        },
+        {
+            id: 2,
+            name: "Products B",
+        }
+]
+function getProducts(product: Product[]):string {
+    const data = product.map(product => product.name);
+    return data.join('');
+}
+const data = getProducts(product);
+console.log(data);
