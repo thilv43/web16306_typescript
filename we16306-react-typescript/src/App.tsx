7ba9aaa54674d1f2c97b504133eaa9c3dbc12343
thilv43 <thilv44443333@gmail.com>
2022-03-22 11:16:36 +0700

    buoi 5

---
+++ we16306-react-typescript/src/App.tsx
@@ -0,0 +1,85 @@
+import { useEffect, useState } from 'react'
+import { Routes, Route, NavLink, Navigate } from 'react-router-dom';
+import logo from './logo.svg'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./dashboard.css";
+
+import ShowInfo from './components/ShowInfo'
+import Product from './components/Product'
+import { add, list, remove } from './api/product';
+import axios from 'axios';
+import type { IProduct } from './types/product';
+import AdminLayout from './pages/layouts/AdminLayout';
+import WebsiteLayout from './pages/layouts/WebsiteLayout';
+import Dashboard from './pages/Dashboard';
+import ProductManager from './pages/ProductManager';
+import Home from './pages/Home';
+import ProductDetail from './pages/ProductDetail';
+import ProductAdd from './pages/ProductAdd';
+
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+        const { data } = await list();
+        setProducts(data);
+    };
+    getProducts();
+  }, [])
+  const removeItem = (id: number) => {
+      // call api
+      remove(id);
+      // reRender
+      setProducts(products.filter(item => item.id !== id));
+  }
+
+  const onHandleAdd = async (product: IProduct) => {
+    const { data } = await add(product);
+    setProducts([...products, data]);
+  }
+  return (
+    <div className="App">
+        <header>
+          <ul>
+            <li>
+              <NavLink to="/">Home Page</NavLink>
+            </li>
+            <li>
+              <NavLink to="/product">Product Page</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">About</NavLink>
+            </li>
+          </ul>
+        </header>
+        <main>
+          
+          <Routes>
+            <Route path="/" element={<WebsiteLayout />}>
+              <Route index element={<Home />} />
+              <Route path="product">
+                  <Route index  element={<h1>Hien thi san pham</h1>} />
+                  <Route path=":id" element={<ProductDetail />} />
+              </Route>
+              <Route path="about" element={<h1>About page</h1>} />
+            </Route>
+            
+            <Route path="admin" element={<AdminLayout />}>
+                <Route index element={<Navigate to="dashboard"/>} />
+                <Route path="dashboard" element={<Dashboard />} />
+                <Route path="products">
+                    <Route index element={<ProductManager products={products} onRemove={removeItem}/>} />
+                    <Route path="add" element={<ProductAdd name="Dat" onAdd={onHandleAdd}/>} />
+                </Route>
+            </Route>
+          </Routes>
+        </main>
+    </div>
+  )
+}
+
+export default App
+
---
+++ we16306-react-typescript/src/api/product.tsx
@@ -0,0 +1,19 @@
+import { IProduct } from "../types/product";
+import instance from "./instance";
+
+export const list = () => {
+    const url = "/products";
+    return instance.get(url);
+}
+export const remove = (id: number) => {
+    const url = `/products/${id}`;
+    return instance.delete(url);
+}
+export const read = (id: string | undefined) => {
+    const url = `/products/${id}`;
+    return instance.get(url);
+}
+export const add = (product: IProduct) => {
+    const url = "/products";
+    return instance.post(url, product);
+}
